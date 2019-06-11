8fce80556ccc4d78b4fd07781de58bc9b1b1546e
mkinzer1 <51351305+mkinzer1@users.noreply.github.com>
2019-06-10 21:42:28 -0700

    Add files via upload

---
+++ hello world.R
@@ -0,0 +1,6 @@
+print("i do not know what i am doing")
+print("hoping this makes it to github")
+print("this file was created within R Studio")
+print("And now it lives on GitHub")
+##this is a markdown file
+hello-world.md
